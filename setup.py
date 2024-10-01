9daf6377cec9c411cb433303d593ebce3c2ed7b4
munechika-koyo <munechika.koyo@gmail.com>
2024-10-01 15:20:40 +0200

    🔨 Refactor setup.py to remove unused imports and update data file handling

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from pathlib import Path
-from setuptools import setup, find_packages
 
+from setuptools import setup
 
 # Include demos in a separate directory in the distribution as data_files.
 demo_parent_path = Path("share/cherab/demos/iter")
@@ -14,35 +14,6 @@ for item in demos_source.rglob("*"):
 data_files = list(data_files.items())
 
 
-with open("README.md") as f:
-    long_description = f.read()
-
-
 setup(
-    name="cherab-iter",
-    version="0.0.1",
-    license="EUPL 1.1",
-    namespace_packages=['cherab'],
-    description="Cherab spectroscopy framework, Iter submodule",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Education",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Cython",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Physics",
-    ],
-    url="https://github.com/cherab",
-    project_urls=dict(
-        Tracker="https://github.com/cherab/iter/issues",
-        Documentation="https://cherab.github.io/documentation/",
-    ),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    install_requires=['cherab'],
-    include_package_data=True,
+    data_files=data_files,
 )
