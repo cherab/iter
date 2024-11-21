46cf49f90ca98b3ce0eaf63f21c0af069c7791f3
munechika-koyo <munechika.koyo@gmail.com>
2024-11-21 18:03:47 +0100

    Add versioning support to the Cherab ITER subpackage
    and add `__init__.py` for the `observer` subpackage

--- cherab/iter/__init__.py
+++ cherab/iter/__init__.py
@@ -1,1 +1,7 @@
 """The Cherab subpacakge for the ITER tokamak."""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version(__package__)  # type: ignore
