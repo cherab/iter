aff40c7cfe3e459ad12cddb9a0ceaf5318bd779b
munechika-koyo <munechika.koyo@gmail.com>
2025-02-04 01:04:40 +0100

    Ensure cache directory creation in get_cache_path function

--- src/cherab/iter/utils.py
+++ src/cherab/iter/utils.py
@@ -20,4 +20,9 @@ def get_cache_path(path: str) -> Path:
     Path
         The full path to the file or directory in the cache directory.
     """
-    return user_cache_path("cherab/iter") / Path(path)
+    _path = user_cache_path("cherab/iter") / Path(path)
+    if _path.is_dir():
+        _path.mkdir(parents=True, exist_ok=True)
+    else:
+        _path.parent.mkdir(parents=True, exist_ok=True)
+    return _path
